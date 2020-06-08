0381819b3dd508bd9393bde9ca478fd67b071a67
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-08 11:49:22 +0530

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,4 +1,3 @@
-echo '::set-env name=TEMPLATE_REPO_PATH::vivishno/testAction'
-echo '::set-env name=TEMPLATE_FILE::azuredeploy3.json'
+echo '::set-env name=TEMPLATE_FILE::azuredeploy.json'
 echo '::set-env name=RESOURCE_GROUP::ExampleGroup'
 
