95b370da88da65b926f8baa1776c8f622af8b714
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-08 11:50:06 +0530

    Update sampleScript.sh

--- sampleScript.sh
+++ sampleScript.sh
@@ -1,1 +1,1 @@
-az deployment group create --resource-group $RESOURCE_GROUP --template-file file_path/$TEMPLATE_FILE
+az deployment group create --resource-group $RESOURCE_GROUP --template-file $TEMPLATE_FILE
